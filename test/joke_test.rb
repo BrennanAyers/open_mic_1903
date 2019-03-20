c988072b34cc2dcc527e26622d13014beb6d711a
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-03-20 09:16:29 -0600

    Add argument attributes test

--- test/joke_test.rb
+++ test/joke_test.rb
@@ -9,4 +9,10 @@ class JokeTest < Minitest::Test
   def test_it_exists
     assert_instance_of Joke, @joke
   end
+
+  def test_it_has_argument_attributes
+    assert_equal 1, @joke.id
+    assert_equal "Why did the strawberry cross the road?", @joke.setup
+    assert_equal "Because his mother was in a jam.", @joke.punchline
+  end
 end
