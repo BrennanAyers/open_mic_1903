7da462e9a7a2ee6baf91bce9790babc01aa30c44
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-03-20 09:17:03 -0600

    Fix setup typo, Add Joke class skel with attr_readers, init method

--- test/joke_test.rb
+++ test/joke_test.rb
@@ -3,7 +3,7 @@ require 'minitest/pride'
 require '../lib/joke'
 class JokeTest < Minitest::Test
   def setup
-    @joke = Joke.new(1, "Why did the strawberry cross the road?", "Becausehis mother was in a jam.")
+    @joke = Joke.new(1, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
   end
 
   def test_it_exists
