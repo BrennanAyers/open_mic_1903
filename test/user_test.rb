4dc6740af3072bf996d31073d4a61a0917f5ef46
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-03-20 09:47:40 -0600

    Add User tell method tests, Fix require typo in test

--- test/user_test.rb
+++ test/user_test.rb
@@ -1,7 +1,7 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require '../lib/joke'
-require '../lib/sal'
+require '../lib/user'
 
 class UserTest < Minitest::Test
   def setup
@@ -36,4 +36,29 @@ class UserTest < Minitest::Test
     assert_equal @joke_2, @sal.jokes[1]
     assert_equal 2, @sal.jokes.length
   end
+
+  def test_user_can_tell_one_joke_to_another_user
+    @sal.learn(@joke_1)
+    @sal.learn(@joke_2)
+
+    assert_equal [], @ali.jokes
+
+    @sal.tell(@ali, @joke_1)
+
+    assert_equal @joke_1, @ali.jokes[0]
+  end
+
+  def test_user_can_tell_multiple_jokes_to_another_user
+    @sal.learn(@joke_1)
+    @sal.learn(@joke_2)
+
+    assert_equal [], @ali.jokes
+
+    @sal.tell(@ali, @joke_1)
+    @sal.tell(@ali, @joke_2)
+
+    assert_equal @joke_2, @ali.jokes[1]
+    assert_equal 2, @ali.jokes.length
+  end
+
 end
