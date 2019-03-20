2f5967d0acc087d6f0b151dcdb4547d5ebe1f93d
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-03-20 09:27:29 -0600

    Add argument attr test, second Joke for tests

--- test/user_test.rb
+++ test/user_test.rb
@@ -5,11 +5,17 @@ require '../lib/user'
 
 class UserTest < Minitest::Test
   def setup
-    @joke = Joke.new(1, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
+    @joke_1 = Joke.new(1, "Why did the strawberry cross the road?", "Because his mother was in a jam.")
+    @joke_2 = Joke.new(2, "How do you keep a lion from charging?", "Take away its credit cards.")
     @user = User.new("Sal")
   end
 
   def test_it_exists
     assert_instance_of User, @user
+  end
+
+  def test_it_has_argument_attributes
+    assert_equal "Sal", @user.name
+    assert_equal [], @user.jokes
   end
 end
